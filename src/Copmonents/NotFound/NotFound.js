f016463b980170560cc4eaa97535a351301533bf
alhabib911 <abdullahalhabib100@gmail.com>
2022-04-07 20:12:25 +0600

    not found page added

--- src/Copmonents/NotFound/NotFound.js
+++ src/Copmonents/NotFound/NotFound.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './NotFound.css'
 const NotFound = () => {
     return (
-        <div>
+        <div className='not-found'>
             <h1>Page Not Found</h1>
+            <p>This page you looking for could not be found. It might <br />
+have removed, renamed or did not exist in the first place.</p>
         </div>
     );
 };
